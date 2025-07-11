098667ec6b8357e30cb113df8d2c104075aa1b5c
Panya Seth Ou <122097534+Asetou@users.noreply.github.com>
2025-07-10 23:45:49 -0700

    added openai api.
    now prompts and receives responses

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,16 +1,23 @@
 import { inngest } from "./client";
+import { openai, createAgent } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
 
-    // Transcription step
-    await step.sleep("wait-a-moment", "10s");
+  async ({ event }) => {
+    // Create a new agent with a system prompt (you can add optional tools, too)
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system:
+        "You are an expert next.js developer.  You write readable, maintable code. You write simple Next.js  and React snippets.",
+      model: openai({ model: "gpt-4.1-mini" }),
+    });
 
-    // Summary step
-    await step.sleep("wait-a-moment", "5s");
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.value}`,
+    );
 
-    return { message: `Hello ${event.data.email}!` };
-  },
+    return { output};
+  }
 );
